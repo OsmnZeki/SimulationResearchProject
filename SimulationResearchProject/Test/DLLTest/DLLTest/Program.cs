342cb649d0c4e17632d08a58872ee74509f09e8f
Osman Çalışkan <ozekicaliskan@gmail.com>
2022-01-05 02:28:35 +0300

    dll tester

---
+++ SimulationResearchProject/Test/DLLTest/DLLTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace DLLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr screen = ScreenDLL.CreateScreen();
+            if (screen == IntPtr.Zero) return;
+
+            while (!ScreenDLL.ScreenShouldClose(screen))
+            {
+                ScreenDLL.ScreenProcessInput(screen);
+                // render
+                // ------
+                ScreenDLL.ScreenUpdate(screen);
+
+                ScreenDLL.ScreenNewFrame(screen);
+            }
+
+            ScreenDLL.ScreenTerminate(screen);
+        }
+    }
+}
---
+++ SimulationResearchProject/Test/DLLTest/DLLTest/ScreenDLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DLLTest
+{
+    static class ScreenDLL
+    {
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CreateScreen();
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool ScreenShouldClose(IntPtr screen);
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ScreenUpdate(IntPtr screen);
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ScreenNewFrame(IntPtr screen);
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ScreenTerminate(IntPtr screen);
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ScreenReturnWidth(IntPtr screen);
+        [DllImport("RenderProgramDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ScreenProcessInput(IntPtr screen);
+    }
+}
