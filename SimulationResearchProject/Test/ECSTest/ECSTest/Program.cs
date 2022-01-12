091a943c045b5dbd42870a31c271f2d882d03c4c
Osman Çalışkan <ozekicaliskan@gmail.com>
2022-01-12 18:26:51 +0300

    test ecs

---
+++ SimulationResearchProject/Test/ECSTest/ECSTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dalak.Ecs;
+
+namespace ECSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World world = new World();
+            SystemManager systemManager;
+            
+        }
+    }
+}
