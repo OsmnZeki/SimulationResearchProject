2caed17783b1f78d7e47164024a2e86efd6e4809
Osman Çalışkan <ozekicaliskan@gmail.com>
2022-05-15 01:13:47 +0300

    modelspath

--- SimulationResearchProject/SimulationWFA/SimulationWFA/MespSimulationSystem/ProgramLibrary/ECSEntegration/SerializedComponent/SkinnedMeshSerialized.cs
+++ SimulationResearchProject/SimulationWFA/SimulationWFA/MespSimulationSystem/ProgramLibrary/ECSEntegration/SerializedComponent/SkinnedMeshSerialized.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dalak.Ecs;
+using ProgramLibrary;
 using RenderLibrary.Graphics;
 using SimulationSystem.ECSComponents;
 
@@ -15,7 +16,7 @@ namespace TheSimulation.SerializedComponent
 
         public override void AddComponent(Entity entity, World world)
         {
-            var model = ModelLoader.LoadModel(modelPath);
+            var model = ModelLoader.LoadModel(SimPath.ModelsPath + "/" + modelPath);
 
             ref var skinnedMesh = ref entity.AddComponent<SkinnedMeshRendererComp>();
             skinnedMesh = new SkinnedMeshRendererComp {
